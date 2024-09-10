e7f3dfe9b861d07d919c619a17d22082924e608c
carlosalvidrez <carlosalvidrez@alum.mit.edu>
2024-09-10 00:10:09 -0400

    [TEX-15825] Modify the library code and commit

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'carlosalvidrez:pagination',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Meteor 3.0 pagination, forked from kurounin:pagination',
   git: 'https://github.com/carlosalvidrez/pagination.git',
   documentation: 'README.md'
@@ -10,10 +10,8 @@ Package.onUse((api) => {
     api.versionsFrom('2.16');
     api.use([
         'ecmascript',
-        //'meteor-base',
         'check',
         'underscore',
-        //'mongo',
     ]);
     api.mainModule('server/pagination.js', 'server');
     api.use([
@@ -23,10 +21,3 @@ Package.onUse((api) => {
     ], 'client');
     api.mainModule('client/pagination.js', 'client');
 });
-
-// Package.onTest(function(api) {
-//   api.use('ecmascript');
-//   api.use('tinytest');
-//   api.use('carlosalvidrez:pagination');
-//   api.mainModule('pagination-tests.js');
-// });
